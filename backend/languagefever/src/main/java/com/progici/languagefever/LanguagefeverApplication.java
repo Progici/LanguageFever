41ad1bd467a2466a3c6f96c8510b378e1542cecf
Dario <dario.sucevac@gmail.com>
2024-11-02 17:06:29 +0100

    springboot start

---
+++ backend/languagefever/src/main/java/com/progici/languagefever/LanguagefeverApplication.java
@@ -0,0 +1,13 @@
+package com.progici.languagefever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagefeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagefeverApplication.class, args);
+	}
+
+}
---
+++ backend/languagefever/src/test/java/com/progici/languagefever/LanguagefeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progici.languagefever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagefeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
