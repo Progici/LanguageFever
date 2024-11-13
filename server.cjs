d804c6ceae473e5df2cd76c4f73b186ef99be57c
jp549576 <162626722+jp549576@users.noreply.github.com>
2024-11-13 22:51:27 +0100

    bug fix and added the right port

--- server.cjs
+++ server.cjs
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5137;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
