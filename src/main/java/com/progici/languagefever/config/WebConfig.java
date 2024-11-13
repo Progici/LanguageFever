e1e8dfacc9b494d7e9cfdbdb285fa7a41261ba2b
jp549576 <162626722+jp549576@users.noreply.github.com>
2024-11-13 22:58:08 +0100

    whitelisted heroku frontend app

--- src/main/java/com/progici/languagefever/config/WebConfig.java
+++ src/main/java/com/progici/languagefever/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://your-frontend-app.herokuapp.com", "http://localhost:5173")
+                        .allowedOrigins("http://localhost:3000", "https://progici2front-62a5e06d95e8.herokuapp.com/", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
