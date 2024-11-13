ab3811290985c926675c25537f583988530819a0
jp549576 <162626722+jp549576@users.noreply.github.com>
2024-11-13 22:55:01 +0100

    bug fix in server.cjs

--- server.cjs
+++ server.cjs
@@ -1,12 +1,8 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5137;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
