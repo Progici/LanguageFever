ecb86d317f8aa0ae4e4bba633d9ae199d0999f9c
jp549576 <162626722+jp549576@users.noreply.github.com>
2024-11-13 21:27:25 +0100

    Update UciteljController.java

--- src/main/java/com/progici/languagefever/controller/UciteljController.java
+++ src/main/java/com/progici/languagefever/controller/UciteljController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") 
 public class UciteljController {
 
   @Autowired
