8122dcde7618f7535acc0502505861114d60160f
tk54015 <tk54015@fer.hr>
2024-11-14 07:18:49 +0100

    jezici

--- src/main/java/com/progici/languagefever/model/enums/Jezici.java
+++ src/main/java/com/progici/languagefever/model/enums/Jezici.java
@@ -1,7 +1,15 @@
 package com.progici.languagefever.model.enums;
 
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+
 public enum Jezici {
   ENGLISH,
   GERMAN,
   SPANISH,
+  MANDARIN,
+  POTRUGUESE,
+  RUSSIAN,
+  JAPANESE,
+  TURKISH,
+  FRENCH
 }
