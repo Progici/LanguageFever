0dff686e2456828c6e851820f62a434cc683c198
mk55225 <mk55225@fer.hr>
2024-11-14 19:31:29 +0100

    Update Home.jsx

--- frontend/my-react-app/src/components/Home.jsx
+++ frontend/my-react-app/src/components/Home.jsx
@@ -3,20 +3,26 @@ import homeImage from '../assets/images/homeimage.png';
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+
 function Home() {
   return (
     <>
       <div className="home">
-        
-        <div className="home-section">
-          <section className="hero-section">
-            {/* Hero sekcija koja prikazuje sliku i tekst */}
-            <div className="home-picture-div">
-              <img src={homeImage} className="home-picture"/> 
-            </div>
-            <section className="hero-text">
-              <h1>Zaronite u svijet stranih jezika uz LanguageFever</h1>
-              <p>Povezujemo učenike i učitelje stranih jezika diljem svijeta</p> 
+          <div className="home-section">
+            <section className="hero-section">
+              <div className="home-picture-div">
+                <img src={homeImage} className="home-picture"/>  
+              </div>
+              <section className="hero-text">
+                <h1>Zaronite u svijet stranih jezika uz LanguageFever</h1>
+                <p>Povezujemo učenike i učitelje stranih jezika diljem svijeta</p> 
+              </section>
+              <section className ="hero-buttons">
+                <Link to="/login">
+                  <button className="cta-button">Započni</button>
+                </Link>
+                <button className="cta-button">O nama</button>
+              </section>
             </section>
             
             <section className="popular-section">
@@ -26,63 +32,55 @@ function Home() {
                   <h3>John Doe</h3> 
                   <img src={teacherPhoto} className="teach-photo"/>
                   <p>★★★★★</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
                 <div className="card">
                   <h3>Jane Doe</h3>
                   <img src={teacherPhoto} className="teach-photo"/> 
                   <p>★★★★★</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
                 <div className="card">
                   <h3>John Doe</h3>
                   <img src={teacherPhoto} className="teach-photo"/> 
                   <p>★★★★☆</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
                 <div className="card">
                   <h3>Jane Doe</h3>
                   <img src={teacherPhoto} className="teach-photo"/> 
                   <p>★★★★☆</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
                 <div className="card">
                   <h3>John Doe</h3>
                   <img src={teacherPhoto} className="teach-photo"/> 
                   <p>★★★★☆</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
                 <div className="card">
                   <h3>Jane Doe</h3>
                   <img src={teacherPhoto} className="teach-photo"/> 
                   <p>★★★★☆</p> 
-                  <p className="language">Jezik</p>
+                  <p>Jezik</p>
                 </div>
               </div>
-              <Link to="/teachers" className="teacher-list-link">            
-                  <h3 className="teacher-list">Pogledaj više</h3>
-              </Link>
             </section>
-          </section>
-          
-          <section className="testimonials-section">
-            {/* Sekcija za prikazivanje korisničkih recenzija */}
-            <h2>Iskustva naših korisnika</h2>
-            <div className="testimonial">
-              {/* Recenzija korisnika */}
-              <p>"Language Fever je najbolja aplikacija!!!"</p>
-              {/* Tekst recenzije */}
-              <span>- Sretni korisnik</span>
-              {/* Ime korisnika koji je dao recenziju */}
-            </div>
-          </section>
+            
+            <section className="testimonials-section">
+              <h2>Iskustva naših korisnika</h2>
+              <div className="testimonial">
+                <p>"Language Fever je najbolja aplikacija!!!"</p>
+                <span>- Sretni korisnik</span>
+              </div>
+            </section>
 
-          <footer className="footer">
-            <p>&copy; 2024 Language Fever. All rights reserved.</p>
-          </footer>
+            <footer className="footer">
+              <p>&copy; 2024 Language Fever. All rights reserved.</p>
+            </footer>
+          </div>
         </div>
-      </div>
-    </>
+      </>
   );
 }
 
