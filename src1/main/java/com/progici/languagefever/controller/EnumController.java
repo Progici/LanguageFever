44d0e93989abd817f2a265843ea23a506c4538bb
Dario776 <Dario.Sucevac@fer.hr>
2024-12-27 12:57:20 +0100

    enum fix

--- src1/main/java/com/progici/languagefever/controller/EnumController.java
+++ src1/main/java/com/progici/languagefever/controller/EnumController.java
@@ -3,36 +3,56 @@ package com.progici.languagefever.controller;
 import com.progici.languagefever.model.enums.Jezici;
 import com.progici.languagefever.model.enums.Kvalifikacija;
 import com.progici.languagefever.model.enums.Razina;
+import com.progici.languagefever.model.enums.Status;
 import com.progici.languagefever.model.enums.Stil;
-import java.io.ObjectInputFilter.Status;
+import java.util.Arrays;
+import java.util.Comparator;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/enums")
 public class EnumController {
 
-  @GetMapping("/liste/jezici")
-  public Jezici[] getJezici() {
-    return Jezici.values();
-  }
-
-  @GetMapping("/liste/kvalifikacija")
-  public Kvalifikacija[] getKvalifikacija() {
-    return Kvalifikacija.values();
-  }
-
-  @GetMapping("/liste/razina")
-  public Razina[] geRazina() {
-    return Razina.values();
-  }
-
-  @GetMapping("/liste/status")
-  public Status[] getStatus() {
+  @GetMapping("/statusi")
+  public Status[] getAllStatuses() {
     return Status.values();
   }
 
-  @GetMapping("/liste/stil")
-  public Stil[] getStil() {
+  @GetMapping("/jeziciDefault")
+  public Jezici[] getAllJeziciByNumberOfPeopleSpeaking() {
+    return Jezici.values();
+  }
+
+  @GetMapping("/jeziciAZ")
+  public Jezici[] getAllJeziciAlphabetical() {
+    return Arrays
+      .stream(Jezici.values())
+      .sorted(Comparator.comparing(Jezici::name))
+      .toArray(Jezici[]::new);
+  }
+
+  @GetMapping("/jeziciZA")
+  public Jezici[] getAllJeziciReverseAlphabetical() {
+    return Arrays
+      .stream(Jezici.values())
+      .sorted(Comparator.comparing(Jezici::name).reversed())
+      .toArray(Jezici[]::new);
+  }
+
+  @GetMapping("/kvalifikacije")
+  public Kvalifikacija[] getAllKvalifikacije() {
+    return Kvalifikacija.values();
+  }
+
+  @GetMapping("/razine")
+  public Razina[] getAllRazina() {
+    return Razina.values();
+  }
+
+  @GetMapping("/stilovi")
+  public Stil[] getAllStilovi() {
     return Stil.values();
   }
 }
--- src1/main/java/com/progici/languagefever/repository/UceniciRepository.java
+++ src1/main/java/com/progici/languagefever/repository/UceniciRepository.java
@@ -3,7 +3,6 @@ package com.progici.languagefever.repository;
 import com.progici.languagefever.model.Ucenik;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.progici.languagefever.repository.KorisniciRepository;
 
 @Repository
 public interface UceniciRepository extends JpaRepository<Ucenik, Long> {}
