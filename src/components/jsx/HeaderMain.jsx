7543a236d2d79653d68a4f0c39fadb4dfb77dc55
mk55225 <mk55225@fer.hr>
2025-01-02 12:47:32 +0100

    Header i faqs dovršeni
    Responzivnost faqs, headera, uklonjen search bar, dodan logout, podešen hamburger

--- src/components/jsx/HeaderMain.jsx
+++ src/components/jsx/HeaderMain.jsx
@@ -1,103 +1,99 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { FaSearch, FaUserCircle, FaBars } from "react-icons/fa";
+import { FaUserCircle, FaBars } from "react-icons/fa";
 import Button from "react-bootstrap/Button";
 import "../css/HeaderMain.css";
 
 function HeaderMain({ active }) {
-  // Stanje za kontrolu prikaza navigacije na manjim ekranima
   const [showMenu, setShowMenu] = useState(false);
 
-  // prop 'active' koji određuje da li je korisnik prijavljen ili ne
   return (
     <Navbar bg="light" data-bs-theme="light" className="header">
-      {/* Navigacijski bar */}
       <Container>
         <Navbar.Brand as={Link} to="/">
           LanguageFever
         </Navbar.Brand>
 
-        {/* Hamburger ikona za manje ekrane */}
-        <div className="hamburger-icon" onClick={() => setShowMenu(!showMenu)}>
-          <FaBars size={24} />
-        </div>
-
-        {/* Navigacija - prikazuje se samo kad je showMenu true na manjim ekranima */}
-        <Nav
-          className={`me-auto navigation ${showMenu ? "show" : ""}`}
-          id="navigation"
-        >
-          <Nav.Link as={Link} to="/teachers">
-            Predavači
-          </Nav.Link>
-          {/* Link koji vodi do stranice s predavačima */}
-          <Nav.Link as={Link} to="/lessons">
-            Lekcije
-          </Nav.Link>
-          {/* Link koji vodi do stranice s lekcijama */}
-          <Nav.Link as={Link} to="/faqs">
-            FAQs
-          </Nav.Link>
-          {/* link koji vodi do stranice s čestim pitanjima */}
-        </Nav>
-
-        <div className="d-flex align-items-center">
-          <div className="search-container">
-            {/* Kontejner za ikonu pretrage i polje za pretragu */}
-            <button className="icon-button">
-              <FaSearch />
+        {/* Profilna ikona za prijavljenog korisnika */}
+        {active ? (
+          <div className="profile-container">
+            <button id="profile-pic">
+              <FaUserCircle size={30} />
             </button>
-            <input
-              type="search"
-              placeholder="Pretraži"
-              className="search-input"
-            />
+            <div className="dropdown-menu">
+              <ul>
+                <li className="d-grid">
+                  <Link to="/editUser" className="link-underline-opacity-0">
+                    <button className="btn btn-primary" id="logout">
+                      Uredi profil
+                    </button>
+                  </Link>
+                </li>
+                <li className="d-grid">
+                  <Link to="/calendar">
+                    <button className="btn btn-primary" id="logout">
+                      Kalendar
+                    </button>
+                  </Link>
+                </li>
+                <li className="d-grid">
+                  <Link to="/logout">
+                    <button className="btn btn-primary" id="logout">
+                      Log Out
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+
+        {/* Hamburger ikona za navigaciju */}
+        <div className="hamburger-container">
+          <div className="hamburger-icon" onClick={() => setShowMenu(!showMenu)}>
+            <FaBars size={24} />
           </div>
 
-          {active ? (
-            // Ako je korisnik prijavljen (prop 'active' je true)
-            <div className="profile-container">
-              <button id="profile-pic">
-                <FaUserCircle size={30} />
-              </button>
-              <div className="dropdown-menu">
-                {/* Dropdown menu koji se prikazuje nakon klika na profilnu ikonu */}
-                <ul>
-                  <li id="option">
-                    <Link to="/editUser" className="link-underline-opacity-0">
-                      Uredi profil
-                    </Link>
-                    {/* Link za uređivanje korisničkog profila */}
-                  </li>
-                  <li id="option">
-                    <Link to="/calendar">Kalendar</Link>
-                    {/* Link za prikazivanje kalendara */}
-                  </li>
-                  {/* <li className="d-grid">
-                    <Link to="/">
-                      <button className="btn btn-primary" id="logout">
-                        Log Out
-                      </button>
-                      { Gumb za odjavu korisnika }
-                    </Link>
-                  </li> */}
-                </ul>
-              </div>
-            </div>
-          ) : (
-            // Ako korisnik nije prijavljen (prop 'active' je false)
-            <div className="d-flex">
-              {/* Div za korisnike koji nisu prijavljeni */}
-              <Link to="/login">
-                {/* Link za prijavu */}
-                <Button className="btn-login">Log In</Button>
-                {/* Gumb za prijavu */}
-              </Link>
-            </div>
-          )}
+          {showMenu && (
+          <div className="hamburger-menu">
+            <ul>
+              {!active && ( // Log In gumb samo ako korisnik nije prijavljen
+                <li className="d-grid">
+                  <Link to="/login" className="link-underline-opacity-0">
+                    <button className="btn btn-primary" id="logout">
+                      Log In
+                    </button>
+                  </Link>
+                </li>
+              )}
+              <li className="d-grid">
+                <Link to="/teachers">
+                  <button className="btn btn-primary" id="logout">
+                    Predavači
+                  </button>
+                </Link>
+              </li>
+              <li className="d-grid">
+                <Link to="/lessons">
+                  <button className="btn btn-primary" id="logout">
+                    Lekcije
+                  </button>
+                </Link>
+              </li>
+              <li className="d-grid">
+                <Link to="/faqs">
+                  <button className="btn btn-primary" id="logout">
+                    FAQs
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
         </div>
       </Container>
     </Navbar>
