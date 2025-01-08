5cbb1952948f1581fecb9a59d12b000b07c05966
mk55225 <mk55225@fer.hr>
2025-01-08 18:50:04 +0100

    header final

--- src/components/jsx/HeaderMain.jsx
+++ src/components/jsx/HeaderMain.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Hamburger from 'hamburger-react'
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { FaUserCircle, FaBars } from "react-icons/fa";
 import Button from "react-bootstrap/Button";
 import "../css/HeaderMain.css";
 
 function HeaderMain({ active }) {
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  const [isOpen, setOpen] = useState(false);
+
+  // Efekt za zatvaranje hamburger izbornika
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <Navbar bg="light" data-bs-theme="light" className="header">
@@ -16,9 +35,22 @@ function HeaderMain({ active }) {
           LanguageFever
         </Navbar.Brand>
 
+        <Nav className="navigation">
+          <Nav.Link as={Link} to="/teachers">
+            Predavači
+          </Nav.Link>
+          <Nav.Link as={Link} to="/lessons">
+            Lekcije
+          </Nav.Link>
+            {/* Link koji vodi do stranice s lekcijama */}
+          <Nav.Link as={Link} to="/faqs">
+            FAQs
+          </Nav.Link>
+        </Nav>
+
         {/* Profilna ikona za prijavljenog korisnika */}
         {active ? (
-          <div className="profile-container">
+          <div className="profile-container1">
             <button id="profile-pic">
               <FaUserCircle size={30} />
             </button>
@@ -49,49 +81,91 @@ function HeaderMain({ active }) {
             </div>
           </div>
         ) : (
-          <div></div>
+          <div className="login-button">
+              {/* Div za korisnike koji nisu prijavljeni */}
+              <Link to="/login">
+                {/* Link za prijavu */}
+                <Button className="btn-login">Log In</Button>
+                {/* Gumb za prijavu */}
+              </Link>
+            </div>
         )}
 
         {/* Hamburger ikona za navigaciju */}
-        <div className="hamburger-container">
-          <div className="hamburger-icon" onClick={() => setShowMenu(!showMenu)}>
-            <FaBars size={24} />
+        <div className="hamburger-container" ref={menuRef}>
+          <div className="hamburger-icon" onClick={() => setShowMenu((prev) => !prev)}>
+            <Hamburger size={24} direction="right" toggled={isOpen} toggle={setOpen}/>
           </div>
 
           {showMenu && (
           <div className="hamburger-menu">
             <ul>
-              {!active && ( // Log In gumb samo ako korisnik nije prijavljen
-                <li className="d-grid">
-                  <Link to="/login" className="link-underline-opacity-0">
-                    <button className="btn btn-primary" id="logout">
-                      Log In
-                    </button>
-                  </Link>
-                </li>
-              )}
+              
               <li className="d-grid">
                 <Link to="/teachers">
-                  <button className="btn btn-primary" id="logout">
+                  <button className="btn btn-primary" id="logout2">
                     Predavači
                   </button>
                 </Link>
               </li>
               <li className="d-grid">
                 <Link to="/lessons">
-                  <button className="btn btn-primary" id="logout">
+                  <button className="btn btn-primary" id="logout2">
                     Lekcije
                   </button>
                 </Link>
               </li>
               <li className="d-grid">
                 <Link to="/faqs">
-                  <button className="btn btn-primary" id="logout">
+                  <button className="btn btn-primary" id="logout2">
                     FAQs
                   </button>
                 </Link>
               </li>
-            </ul>
+              {!active && ( // Log In gumb samo ako korisnik nije prijavljen
+                <li className="d-grid">
+                  <Link to="/login" className="link-underline-opacity-0">
+                    <button className="btn btn-primary" id="logout2">
+                      Log In
+                    </button>
+                  </Link>
+                </li>
+              )}
+              {active && ( // profil gumb samo ako korisnik je prijavljen
+                <li className="d-grid">
+                  <button id="profile-pic">
+                    <FaUserCircle size={30} className="hambi-user-icon" />
+                  </button>
+                </li>
+              )}
+              {active && (
+                <li className="d-grid">
+                  <Link to="/editUser" className="link-underline-opacity-0">
+                    <button className="btn btn-primary" id="logout2">
+                      Uredi profil
+                    </button>
+                  </Link>
+                </li>
+              )}
+              {active && (
+                <li className="d-grid">
+                  <Link to="/calendar">
+                    <button className="btn btn-primary" id="logout2">
+                      Kalendar
+                    </button>
+                  </Link>
+                </li>
+              )}
+              {active && (
+                <li className="d-grid">
+                  <Link to="/logout">
+                    <button className="btn btn-primary" id="logout2">
+                      Log Out
+                      </button>
+                    </Link>
+                </li>
+              )}
+            </ul> 
           </div>
         )}
         </div>
