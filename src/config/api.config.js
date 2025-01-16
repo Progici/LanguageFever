3e598b9fba36792d80aab42c311cbb4696eda09e
jp549576 <162626722+jp549576@users.noreply.github.com>
2025-01-16 21:09:59 +0100

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -3,5 +3,5 @@ dotenv.config();
 
 export const ApiConfig = {
   // Use the environment variable for the API URL
-  API_URL: process.env.BACKEND_URL,
+  API_URL: "https://progici2-54ed215166d3.herokuapp.com",
 };
